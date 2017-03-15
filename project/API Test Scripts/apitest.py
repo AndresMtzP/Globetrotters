e9c19b029d8e9ecf18d10513366c4f9a5ac1c6e8
khhang <brian.qk.pham@gmail.com>
2017-03-14 22:11:19 -0700

    Reorganized folders, switched to Google Places for images, added extra functions for Teleport API
    Changed the getImage() function to use Google's API. Added some extra
    functions to the Teleport API to get city information and create
    different files (csv, txt) for our locations. Added the cities, states,
    and countries list which contains all locations that the Teleport API
    references. Reorganized directory to show scripts that are currently not
    being worked on in the API Test Scripts folder.

---
+++ project/API Test Scripts/apitest.py
@@ -0,0 +1,237 @@
+# apitest.py - This is a script to test API calls to various API's to get location specific information
+import requests
+import MySQLdb
+import teleportapi
+# from datafinder import getGeneral, getImage
+import re
+
+# This function will make a GET request to Wikipedia's endpoint and parse the response
+# for the first paragraph of the page
+def wikiSum(loc):
+	r = requests.get("https://en.wikipedia.org/w/api.php?format=json&action=query&prop=extracts&exintro=&explaintext=&titles="+loc)
+	data = r.json()
+	pageid = data['query']['pages'].keys()[0]
+	print data['query']['pages'][pageid]['extract'].encode("utf-8")
+
+# This function will make a GET request to APIXU API to get current weather information.
+# This API requires an API key and has a limited # of calls
+def weatherInfo(loc, key):
+	r = requests.get("http://api.apixu.com/v1/current.json?key="+key+"&q="+loc)
+	data = r.json();
+	print "After weather get request."
+	print "Location Name: %s" % data['location']['name'].encode("utf-8")
+	print "Location Region: %s" % data['location']['region'].encode("utf-8")
+	print "Location Country: %s" % data['location']['country'].encode("utf-8")
+	print "Local Time: %s" % data['location']['localtime'].encode("utf-8")
+	print "Temperature(F): %f" % data['current']['temp_f'] # value is a float
+	print "Weather: %s" % data['current']['condition']['text']
+
+# This function will make a GET request to rescountries.eu and parse the response
+# for various information about a country
+def getAllCountries():
+	# REST Countries
+	r = requests.get("https://restcountries.eu/rest/v1/all")
+	data = r.json()
+	print '{:40}{:20}{:30}{:20}{:30}{:25}{:25}'.format("Country","Population","Capital","Region",
+		"Sub Region", "Lat.", "Long.")
+	for i in range(len(data)):
+		if data[i]['latlng']:
+			lat = data[i]['latlng'][0]
+			lng =  data[i]['latlng'][1]
+		else:
+			lat = ""
+			lng = ""
+		print '{:40}{:<20}{:<30}{:<20}{:<30}{:<25}{:<25}'.format(data[i]['name'].encode("utf-8"),data[i]['population'],
+			data[i]['capital'].encode("utf-8"),data[i]['region'].encode("utf-8"),data[i]['subregion'].encode("utf-8"),
+			lat,lng)
+
+# This function will make a call to restcountries.eu and populate the Countries table in the RESTCountries database
+def createCountriesTable():
+	# establish connection to MySQL database
+	db = MySQLdb.connect(host='192.168.1.101', user='root', passwd='globetrotters', db='RESTCountries')
+	cur = db.cursor()
+	print "Connected to MySQL DB"
+	
+	# delete all existing rows and reset auto increment
+	cur.execute("DELETE FROM Countries")
+	cur.execute("ALTER TABLE Countries AUTO_INCREMENT = 1")
+	print "Reset table rows"
+
+	# get response from restcountries api and place column data in a tuple
+	r = requests.get("https://restcountries.eu/rest/v1/all")
+	data = r.json()
+	cur.execute("SET NAMES utf8;")
+	for i in range(len(data)):
+		# validate that lat/lng values exist in response for location
+		if data[i]['latlng']:
+			lat = data[i]['latlng'][0]
+			lng =  data[i]['latlng'][1]
+		else:
+			lat = ""
+			lng = ""
+		name = data[i]['name'].encode("utf-8")
+		pop = data[i]['population']
+		capital = data[i]['capital'].encode("utf-8")
+		region = data[i]['region'].encode("utf-8")
+		subregion = data[i]['subregion'].encode("utf-8")
+		row = (name, pop, capital, region, subregion, lat, lng)
+		cur.execute("INSERT INTO Countries (CountryName, Population, Capital, Region, SubRegion, Lat, Lng) VALUES (%s, %s, %s, %s, %s, %s, %s)", row)
+	print "Populated table"	
+
+	# commit and close connection
+	db.commit()
+	cur.close()
+	db.close()
+	print "Closed the connection"
+
+# script to update the tables using REST countries API
+def updateCountriesTable():
+	# establish connection to MySQL database
+	db = MySQLdb.connect(host='192.168.1.101', user='root', passwd='globetrotters', db='RESTCountries')
+	cur = db.cursor()
+	print "Connected to MySQL DB"
+
+	# find entry in table and check its values if it exists, if they're not the same then update
+	r = requests.get("https://restcountries.eu/rest/v1/all")
+	data = r.json()
+	for i in range(len(data)):
+		if data[i]['latlng']:
+			lat = data[i]['latlng'][0]
+			lng =  data[i]['latlng'][1]
+		else:
+			lat = ""
+			lng = ""
+		name = data[i]['name'].encode("utf-8")
+		pop = data[i]['population']
+		capital = data[i]['capital'].encode("utf-8")
+		region = data[i]['region'].encode("utf-8")
+		subregion = data[i]['subregion'].encode("utf-8")
+		info = (name, pop, capital, region, subregion, lat, lng)
+		cur.execute("SELECT * FROM Countries WHERE CountryName = %s", loc)
+		print "Found %s row(s)" % cur.rowcount
+		if cur.rowcount > 0:
+			row = cur.fetchone()
+			if row != info:
+				updateRow(cur, row, info)
+		else:
+			cur.execute("INSERT INTO Countries (CountryName, Population, Capital, Region, SubRegion, Lat, Lng) VALUES (%s, %s, %s, %s, %s, %s, %s)", info)
+			print "Added %s to table" % name
+
+
+	# commit and close connection
+	db.commit()
+	cur.close()
+	db.close()
+	print "Closed the connection"
+
+def updateCountryRow(cur, row, info):
+	for i in range(len(cur.description)):
+		cur.execute("UPDATE Countries SET %s = %s WHERE CountryName = %s", (cur.description[i][0], row[i], row[0]))
+	print "Updated %s row" % row[0]
+
+
+
+# This function will get all the information using the MySQL database and/or API
+def getInfo(loc):
+	# establish connection to MySQL database
+	db = MySQLdb.connect(host='192.168.1.101', user='root', passwd='globetrotters', db='RESTCountries')
+	cur = db.cursor()
+	print "Connected to MySQL DB"
+
+	# look for location in the database
+	cur.execute("SELECT * FROM Countries WHERE CountryName = %s", loc)
+	print "Found %s row(s)" % cur.rowcount
+	if cur.rowcount > 0:
+		print cur.fetchone()
+
+	# close connection
+	cur.close()
+	db.close()
+	print "Closed the connection"
+
+def createCitiesTable():
+	db = MySQLdb.connect(host='192.168.1.101', user='root', passwd='globetrotters', db='Globetrotters')
+	cur = db.cursor()
+	print "Connected to MySQL DB"
+
+	# delete existing rwows from cities table
+	cur.execute("DELETE FROM Cities")
+	cur.execute("ALTER TABLE Cities AUTO_INCREMENT = 1")
+	print "Reset table rows"
+
+	cur.execute("SET NAMES utf8;")
+	# get city information from text file
+	f = open("allCities.txt", "r")
+	for loc in f.readlines():
+		print loc
+		latlng = teleportapi.getLatLng(loc)
+		lat = latlng[0]
+		lng = latlng[1]
+		row = (loc, teleportapi.getPopulation(loc), lat, lng, teleportapi.getCountry(loc))
+		cur.execute("INSERT INTO Cities (CityName, Population, Lat, Lng, CountryName) VALUES (%s, %s, %s, %s, %s)", row)
+	f.close()
+	print "Populated table"	
+
+	# commit and close connection
+	db.commit()
+	cur.close()
+	db.close()
+	print "Closed the connection"
+
+
+def main():
+	# api key values
+	weatherKey = "159c5b95dadb4378ba8195926161808"
+
+	# regular expression search strings
+	# introRE = "\"extract\":\"(?P<intro>.*)\"}"
+
+	loop = 1
+	# takes raw input data from user and formats string to use in request
+	while loop == 1:
+		print "API Options\n(1) Wiki API\n(2) Weather API\n(3) Get all countries\n(4) Create Countries Table"
+		print "(5) Get all info\n(6) Test update database\n(7) Create Cities DB\n(Q) Quit"
+		option = raw_input("Select option to test response: ")
+		if option == "1":
+			# location options
+			locSearch = raw_input("Enter a location: ")
+			locSearch = locSearch.replace(" ", "%20")
+
+			# obtain wiki summary
+			wikiSum(locSearch)
+			print "After wiki get request."
+		elif option == "2":
+			# weather options
+			weatherSearch = raw_input("Enter a city, zip code, or lat/long: ")
+
+			# obtain weather info
+			weatherInfo(weatherSearch, weatherKey)
+			print "After weather get request."
+		elif option == "3":
+			# REST Countries
+			getAllCountries()
+		elif option == "4":
+			# update DB
+			createCountriesTable()			
+		elif option == "5":
+			# fetch all info for location
+			locSearch = raw_input("Enter a location: ")
+			locSearch = locSearch.replace(" ", "%20")
+			getInfo(locSearch)
+		elif option == "6":
+			# test update tables
+			updateCountriesTable()
+		elif option == "7":
+			createCitiesTable()
+		elif option == "Q":
+			loop = 0
+			print "Exiting program"
+	#print pageid
+	#print r.content
+
+	# parse strings for info
+	#intro = re.search(introRE, r.content)
+	#print "After string was parsed"
+	#print intro.group('intro')
+
+main()
--- project/teleportapi.py
+++ project/teleportapi.py
@@ -6,6 +6,29 @@
 import requests
 import urllib
 
+def getAll(loc):
+	cityName=population=lat=lng=stateName=countryName=None
+	payload = {'search' : loc, 'embed' : 'city:search-results/city:item'}
+	r = requests.get("https://api.teleport.org/api/cities", params=payload)
+	data = r.json()
+	if data['_embedded']['city:search-results']:
+		result = data['_embedded']['city:search-results'][0]['_embedded']['city:item']
+		infoKeys = result.keys()
+		if 'name' in infoKeys:
+			cityName = result['name'].encode('utf-8')
+		if 'population' in infoKeys:
+			population = result['population']
+		if 'location' in infoKeys:
+			lat = result['location']['latlon']['latitude']
+			lng = result['location']['latlon']['longitude']
+		if '_links' in infoKeys:
+			if 'city:country' in result['_links'].keys():
+				countryName = result['_links']['city:country']['name'].encode('utf-8')
+		if '_links' in infoKeys:
+			if 'city:admin1_division' in result['_links'].keys():
+				stateName = result['_links']['city:admin1_division']['name'].encode('utf-8')
+	return (cityName,population,lat,lng,stateName,countryName)
+
 # Gets the population of the city
 def getPopulation(loc):
 	payload = {'search': loc}
@@ -81,12 +104,13 @@ def getSearchResults(loc):
 		print result['matching_full_name'].encode('utf-8')
 
 # Gets the image of the urban area the city is located in
-def getImage(loc):
+def getImage(loc, count):
 	payload = {'search': loc}
 	r = requests.get("https://api.teleport.org/api/cities", params=payload)
 	data = r.json()
-	result = data['_embedded']['city:search-results'][0]
-	r = requests.get(result['_links']['city:item']['href'])
+	if data['_embedded']['city:search-results']:
+		result = data['_embedded']['city:search-results'][0]
+		r = requests.get(result['_links']['city:item']['href'])
 	data = r.json()['_links']
 	matchKeys = data.keys()
 	if 'city:urban_area' in matchKeys:
@@ -94,11 +118,72 @@ def getImage(loc):
 		data = r.json()
 		r = requests.get(data['_links']['ua:images']['href'])
 		data = r.json()
-		urllib.urlretrieve(data['photos'][0]['image']['mobile'],"%s.jpg" % loc)
+		urllib.urlretrieve(data['photos'][0]['image']['mobile'],"C:\Users\Brian Pham\Documents\Classes\COMPE 490\Levitating Globe\API\Images\%s.jpg" % count)
+
+# Gets detailed information about certain location
+def getDetails(loc):
+	weatherType=curr=lifeExpectancy=lang=None
+
+	details = ['CLIMATE', 'ECONOMY', 'INTERNAL', 'LANGUAGE']
+	payload = {'search': loc}
+	r = requests.get("https://api.teleport.org/api/cities", params=payload)
+	data = r.json()
+	if data['_embedded']['city:search-results']:
+		result = data['_embedded']['city:search-results'][0]
+		r = requests.get(result['_links']['city:item']['href'])
+	data = r.json()['_links']
+	matchKeys = data.keys()
+	if 'city:urban_area' in matchKeys:
+		r = requests.get(data['city:urban_area']['href'])
+		data = r.json()['_links']
+		matchKeys = data.keys()
+		if 'ua:details' in matchKeys:
+			r = requests.get(data['ua:details']['href'])
+			data = r.json()['categories']
+			for result in data:
+				if result['id'] in details:
+					ID = result['id']
+					if ID == 'CLIMATE':
+						climate = result['data']
+						for x in climate:
+							if x['id'] == 'WEATHER-TYPE':
+								weatherType = x['string_value']
+								print x['label'] + ": " + weatherType
+					elif ID == 'ECONOMY':
+						econ = result['data']
+						for x in econ:
+							if x['id'] == 'CURRENCY-URBAN-AREA':
+								curr = x['string_value']
+								print x['label'] + ": " + curr
+					elif ID == 'INTERNAL':
+						internal = result['data']
+						for x in internal:
+							if x['id'] == 'LIFE-EXPECTANCY':
+								lifeExpectancy = str(x['float_value'])
+								print x['label'] + ": " + lifeExpectancy
+					elif ID == 'LANGUAGE':
+						lang = result['data']
+						for x in lang:
+							if x['id'] == 'SPOKEN-LANGUAGES':
+								lang = x['string_value']
+								print x['label'] + ": " + lang
+	return (weatherType,curr,lifeExpectancy,lang)
 
 def createCitiesCsv():
 	fr = open("allCities.txt", "r")
 	fw = open("allCities.csv", "w")
+	count = 1
+	for loc in fr.readlines():
+		print loc
+		fw.write("%s,%s,%s,%s,%s,%s,%s\n" % ((count,)+getAll(loc)))
+		count+=1
+	fr.close()
+	fw.close()
+
+# Creates CSV containing cities and general information
+def createStatesCsv():
+	fr = open("allStates.txt", "r")
+	fw = open("allStates.csv", "w")
 	count = 1
 	for loc in fr.readlines():
 		print loc
@@ -109,7 +194,7 @@ def createCitiesCsv():
 		else:
 			lat = None;
 			lng = None;
-		fw.write("%s,%s,%s,%s,%s,%s,%s" % (count, loc.strip(), getPopulation(loc), lat, lng, getCountry(loc), getContinent(loc)))
+		fw.write("%s,%s,%s,%s,%s,%s,%s\n" % (count, loc.strip(), getPopulation(loc), lat, lng, getCountry(loc), getContinent(loc)))
 		count+=1
 	fr.close()
 	fw.close()
@@ -117,19 +202,41 @@ def createCitiesCsv():
 # Writes all the cities that Teleport API references in a file (non-alphabetical)
 def writeAllCities():
 	f = open('allCities.txt', 'w')
+	r = requests.get('https://api.teleport.org/api/countries/?embed=country%3Aitems%2Fcountry%3Aadmin1_divisions')
+	data = r.json()
+	countryList = data['_embedded']['country:items']
+	for country in countryList:
+		stateList = country['_embedded']['country:admin1_divisions']['_links']['a1:items']
+		for state in stateList:
+			r = requests.get(state['href'])
+			data = r.json()['_links']
+			r = requests.get(data['a1:cities']['href'])
+			data = r.json()
+			if 'name' in state.keys():
+				print "Writing cities in %s" % state['name'].encode('utf-8')
+				cityList = data['_links']['city:items']
+				for city in cityList:
+					if 'name' in city.keys():
+						f.write('%s,%s\n' % (city['name'].encode('utf-8'),state['name'].encode('utf-8')))
+	f.close()
+
+
+# Writes all the states that Teleport API references in a file (non-alphabetical)
+def writeAllStates():
+	f = open('allStates.txt', 'w')
 	r = requests.get('https://api.teleport.org/api/countries')
 	data = r.json()
 	countryList = data['_links']['country:items']
 	for country in countryList:
 		r = requests.get(country['href'])
-		print "Writing cities in %s" % country['name']
+		print "Writing states in %s" % country['name']
 		data = r.json()
 		r = requests.get(data['_links']['country:admin1_divisions']['href'])
 		data = r.json()
-		cityList = data['_links']['a1:items']
-		for city in cityList:
-			if 'name' in city.keys():
-				f.write('%s\n' % city['name'].encode('utf-8'))
+		stateList = data['_links']['a1:items']
+		for state in stateList:
+			if 'name' in state.keys():
+				f.write('%s\n' % state['name'].encode('utf-8'))
 	f.close()
 
 # Writes all the countries that Teleport API references in a file (alphabetical)
@@ -144,8 +251,10 @@ def writeAllCountries():
 
 
 def main():
+	
+	#loc = raw_input("Enter a location: ")
+	#print getAll(loc)
 	'''
-	loc = raw_input("Enter a location: ")
 	getFullName(loc)
 	getPopulation(loc)
 	getLatLng(loc)
@@ -158,5 +267,28 @@ def main():
 	# writeAllCities()
 	'''
 	createCitiesCsv()
+	'''
+	f = open('allCities.txt', 'r')
+	count = 1
+	for line in f.readlines():
+		print line
+		getImage(line, count)
+		count += 1
+	f.close()
+	'''
+	'''
+	fr = open("allCities.txt", "r")
+	fw = open("allCities.csv", "w")
+	count = 1
+	for loc in fr.readlines():
+		print loc
+		fw.write("%s,%s,%s,%s,%s\n" % ((count,) + getDetails(loc)))
+		count+=1
+	fr.close()
+	fw.close()
+	'''
+	#writeAllCountries()
+	#writeAllStates()
+	#writeAllCities()
 
 main()
